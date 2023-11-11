da802308ed80bb89089e1c7f213a227d696c9878
Ermak <ermakovila640@gmail.com>
2023-11-11 20:16:57 +0300

    add register page

--- castaway/home/views.py
+++ castaway/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .forms import UserRegisterForm
 
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    form = UserRegisterForm()
+    return render(request, 'registration.html', {'form': form})
