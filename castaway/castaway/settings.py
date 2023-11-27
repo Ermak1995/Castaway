76fa9a51a4bfef70fc97ecda6c8d840f8a56a311
Ermak <ermakovila640@gmail.com>
2023-11-27 20:57:21 +0300

    create table Episodes

--- castaway/castaway/settings.py
+++ castaway/castaway/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'home/static/images')
-MEDIA_URL = '/images/'
+MEDIA_URL = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- castaway/home/models.py
+++ castaway/home/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+
+class Episodes(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField()
+    tags = models.ManyToManyField('Tags')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    author = models.ManyToManyField('Users')
+
