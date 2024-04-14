7f8680f14a12278fec6ca86b292ff67c06f4e619
Ermak <ermakovila640@gmail.com>
2024-04-14 14:22:19 +0300

    add many tags

--- castaway/home/models.py
+++ castaway/home/models.py
@@ -1,14 +1,12 @@
 from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
-# class TagsManager(models.Manager):
-#     def get_queryset(self):
-#         return super().get_queryset().all()
 
 class Episodes(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
     description = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField()
@@ -16,14 +14,19 @@ class Episodes(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    # author = models.ManyToManyField(Author)
 
+    # author = models.ManyToManyField(Author)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('episodes_detail', kwargs={"episode_id": self.pk})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Episodes'
@@ -39,8 +42,19 @@ class Tags(models.Model):
     def get_absolute_url(self):
         return reverse('show_tags', kwargs={"tag_slug": self.slug})
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
     class Meta:
         verbose_name_plural = 'Tags'
+
+from home.models import Tags
+from json import loads
+
+with open(r"D:\Castaway\text.json") as file:
+    l = loads(file.read())
 
 
 
